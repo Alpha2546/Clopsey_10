94acb371e9bb062c62ba4a3f93f0696217a1fb47
Alpha2546 <adriaanvanloon@hotmail.com>
2015-10-01 20:43:47 +0200

    Filling the pack with maps and setting up the modinfo.json

---
+++ ui/mods/cMaps_Clopsey2v2/maplist.js
@@ -0,0 +1,8 @@
+cShareSystems.load_pas("Clopsey 2V2", [
+	"coui://ui/mods/cMaps_Clopsey2v2/systems/burnt_earth.pas",
+	"coui://ui/mods/cMaps_Clopsey2v2/systems/gk_proving_grounds.pas",
+	"coui://ui/mods/cMaps_Clopsey2v2/systems/oort_system.pas",
+	"coui://ui/mods/cMaps_Clopsey2v2/systems/aquarius.pas",
+	"coui://ui/mods/cMaps_Clopsey2v2/systems/system_f205.pas",
+	"coui://ui/mods/cMaps_Clopsey2v2/systems/hydrogenix_facility_f407_2v2_titans.pas"	
+]);
