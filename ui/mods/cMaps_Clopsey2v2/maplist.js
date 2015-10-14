2907701cf6da20ddb4f15a99042fd80926e55bca
pieter vanh <pieter.vanh@gmail.com>
2015-10-14 21:25:38 +0200

    Update to 1.3 Remove Aquarius added HJK / and Andreas maps

--- ui/mods/cMaps_Clopsey2v2/maplist.js
+++ ui/mods/cMaps_Clopsey2v2/maplist.js
@@ -2,7 +2,9 @@ cShareSystems.load_pas("Clopsey 2V2", [
 	"coui://ui/mods/cMaps_Clopsey2v2/systems/burnt_earth.pas",
 	"coui://ui/mods/cMaps_Clopsey2v2/systems/gk_proving_grounds.pas",
 	"coui://ui/mods/cMaps_Clopsey2v2/systems/oort_system.pas",
-	"coui://ui/mods/cMaps_Clopsey2v2/systems/aquarius.pas",
 	"coui://ui/mods/cMaps_Clopsey2v2/systems/system_f205.pas",
-	"coui://ui/mods/cMaps_Clopsey2v2/systems/hydrogenix_facility_f407_2v2_titans.pas"	
+	"coui://ui/mods/cMaps_Clopsey2v2/systems/hydrogenix_facility_f407_2v2_titans.pas",
+	"coui://ui/mods/cMaps_Clopsey2v2/systems/hjk-789.pas",
+	"coui://ui/mods/cMaps_Clopsey2v2/systems/gsystem_vs2v2.pas",
+	"coui://ui/mods/cMaps_Clopsey2v2/systems/the_forgery.pas"
 ]);
