740511e197844747ae7162cc44004946a1e3d5de
pieter vanh <pieter.vanh@gmail.com>
2015-12-10 18:00:56 +0100

    Update mappack to 1.4 added Team Dual maps

--- ui/mods/cMaps_Clopsey2v2/maplist.js
+++ ui/mods/cMaps_Clopsey2v2/maplist.js
@@ -6,5 +6,8 @@ cShareSystems.load_pas("Clopsey 2V2", [
 	"coui://ui/mods/cMaps_Clopsey2v2/systems/hydrogenix_facility_f407_2v2_titans.pas",
 	"coui://ui/mods/cMaps_Clopsey2v2/systems/hjk-789.pas",
 	"coui://ui/mods/cMaps_Clopsey2v2/systems/gsystem_vs2v2.pas",
+	"coui://ui/mods/cMaps_Clopsey2v2/systems/fortress.pas",
+	"coui://ui/mods/cMaps_Clopsey2v2/systems/the_t3mple_v2.pas",
+	"coui://ui/mods/cMaps_Clopsey2v2/systems/frozenoreoyoghurt.pas",
 	"coui://ui/mods/cMaps_Clopsey2v2/systems/the_forgery.pas"
 ]);
